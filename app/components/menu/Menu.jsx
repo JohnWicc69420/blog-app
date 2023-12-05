32cc57052a11616f507cea84b6a66ba1b74d426c
JohnWicc69420 <tusmantariq9111@gmail.com>
2023-12-05 21:11:16 +0500

    replaced apostrophe with &rsquo;

--- app/components/menu/Menu.jsx
+++ app/components/menu/Menu.jsx
@@ -225,7 +225,7 @@ const data = [
 const Menu = async () => {
   return (
     <div>
-      <h2 className="text-[#626262] dark:text-[#a6a6a6]">What's Hot</h2>
+      <h2 className="text-[#626262] dark:text-[#a6a6a6]">What&rsquo;s Hot</h2>
       <h1 className="text-3xl font-bold mb-8 pt-1">Most Popular</h1>
       <div className="xl:mb-16 mb-8">
         <MenuPosts data={data} hasImage={false} />
@@ -238,7 +238,7 @@ const Menu = async () => {
       <h2 className="text-[#626262] dark:text-[#a6a6a6]">
         Chosen by the editor
       </h2>
-      <h1 className="text-3xl font-bold mb-8 pt-1">Editor's Pick</h1>
+      <h1 className="text-3xl font-bold mb-8 pt-1">Editor&rsquo;s Pick</h1>
       <div className="xl:pb-16 pb-8">
         <MenuPosts data={data} hasImage={true} />
       </div>
