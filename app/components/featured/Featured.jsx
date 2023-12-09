7fbaaa7853383ee464d4f25379b00dab2ddbf8ce
JohnWicc69420 <tusmantariq9111@gmail.com>
2023-12-09 13:03:31 +0500

    fully done

--- app/components/featured/Featured.jsx
+++ app/components/featured/Featured.jsx
@@ -6,6 +6,7 @@ const Featured = async () => {
   const randNumber = Math.floor(Math.random() * 18);
   const data = await blogs();
   const post = data[randNumber];
+  const id = post._id;
   return (
     <>
       <div className="h-full md:pt-10 pt-4 mb-12">
@@ -16,18 +17,20 @@ const Featured = async () => {
           </div>
         </p>
         <div className="bottom flex items-center pt-10 gap-10">
-          <div
-            className="left rounded-3xl hidden md:flex items-center justify-center overflow-hidden
+          <Link href={`blog/${id}`}>
+            <div
+              className="left cursor-pointer rounded-3xl hidden md:flex items-center justify-center overflow-hidden
           h-[280px] min-w-[350px]
          lg:h-[360px] lg:min-w-[450px] 
          2xl:h-[440px] 2xl:min-w-[550px] "
-          >
-            <img
-              className="object-cover hover:scale-103 transition-pic h-full w-full"
-              src={post?.img}
-              alt=""
-            />
-          </div>
+            >
+              <img
+                className="object-cover hover:scale-103 transition-pic h-full w-full"
+                src={post?.img}
+                alt=""
+              />
+            </div>
+          </Link>
           <div className="right flex flex-col items-start">
             <h1 className=" lg:text-3xl lg:font-bold text-xl line-clamp-2 font-bold">
               {post?.title}
@@ -35,19 +38,7 @@ const Featured = async () => {
             <p className="text-[#a6a6a6] my-4 lg:text-base text-sm line-clamp-6 font-medium">
               {post?.desc}
             </p>
-            <Link
-              href={{
-                pathname: "/blog",
-                query: {
-                  img: post?.img,
-                  date: post?.date,
-                  title: post?.title,
-                  desc: post?.desc,
-                  writer: post?.writer,
-                  writerImg: post?.writerImg,
-                },
-              }}
-            >
+            <Link href={`blog/${id}`}>
               <button
                 className="bg-[#ECECEC] p-3 font-medium rounded-md 
             text-sm text-[#000]"
