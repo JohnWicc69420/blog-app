da0f78ea312e9371ffd1a4d3741bd8a26269af95
JohnWicc69420 <tusmantariq9111@gmail.com>
2023-12-13 20:31:32 +0500

    fixed a few bugs

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -2,9 +2,8 @@ import connectMongoDB from "../../../libs/mongodb";
 import Blog from "../../../model/blog";
 import { NextResponse } from "next/server";
 
-connectMongoDB();
-
 export async function GET() {
+  connectMongoDB();
   const blogs = await Blog.find();
   return NextResponse.json({ blogs });
 }
